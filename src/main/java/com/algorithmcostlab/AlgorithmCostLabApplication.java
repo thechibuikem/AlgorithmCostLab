30e7ddad126a72d98c623c18d25fc1066b5802ab
Chukwuemeka David <david.chukwuemeka.swe@gmail.com>
2026-09-23 01:56:57 +0100

    feat: add main entry point

---
+++ src/main/java/com/algorithmcostlab/AlgorithmCostLabApplication.java
@@ -0,0 +1,11 @@
+package com.algorithmcostlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication 
+public class AlgorithmCostLabApplication {
+    public static void main(String[] args){
+        SpringApplication.run(AlgorithmCostLabApplication.class, args);
+    }
+}
